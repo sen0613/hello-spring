190cee2fd1f85a8299d3f6dcb6ce7d92b14230f3
Sewon <sewon0613@naver.com>
2017-06-16 14:59:38 +0900

    2017-06-16 게시판 구현.. SpringJdbc를 통해 상세보기와 전체보기 구현

--- src/test/java/kr/re/kitri/hello/dao/ArticleDaoSpringJdbcTest.java
+++ src/test/java/kr/re/kitri/hello/dao/ArticleDaoSpringJdbcTest.java
@@ -1,11 +1,14 @@
 package kr.re.kitri.hello.dao;
 
 import kr.re.kitri.hello.model.Article;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * Created by danawacomputer on 2017-06-16.
@@ -19,6 +22,19 @@ public class ArticleDaoSpringJdbcTest {
 
     @Autowired
     private ArticleDaoSpringJdbc articleDao;
+
+    @Test
+    public void testSelectAllArticles() {
+        List<Article> list = articleDao.selectAllArticles();
+        Assert.assertTrue(list.size() > 2);
+    }
+
+    @Test
+    public void testSelectArticleById() {
+        Article article = articleDao.selectArticleById("3");
+        System.out.println();
+        Assert.assertTrue(article.getArticleId().equals("3"));
+    }
 
     @Test
     public void testInsertArticle() {
